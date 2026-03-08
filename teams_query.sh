e1635618a1c6b10ed8345625749c8d94b2563bef
Vanessa E. <56154297+nessiaart@users.noreply.github.com>
2026-03-07 19:35:07 -0500

    Update teams_query.sh

--- teams_query.sh
+++ teams_query.sh
@@ -3,9 +3,9 @@
 #!/bin/sh
 
 # Oracle Database connection information
-USER="akward"
-PASSWORD="05032458"
-CONNECTION_STRING="(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))"
+USER="your_username"
+PASSWORD="your_password"
+CONNECTION_STRING="your_connection_string"
 
 # Use SQL*Plus to run the SQL statements
 # Display all teams with their information and formation (join query)
